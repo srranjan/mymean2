13da1966e10bd3c5794b2aea78da9e5d8dc1b35d
Rajiv Ranjan <rajiv.ranjan@ieee.org>
2018-06-23 07:45:45 -0400

    mymongo restored instead of mongodb and removing some local stuff - hope this works

--- routes/config.js
+++ routes/config.js
@@ -6,6 +6,9 @@
 module.exports = {
     'remoteUrl' : 'http://localhost:9091/clients/',
       //Value below changed from mymong to mongdb-persisten for openshift template requirement
-	'mykubermongo' : 'mongodb',       //Comment out if not using Kubernetes, otherwise this will break the code in other environments
+	//'mykubermongo' : 'mongodb',       //Comment out if not using Kubernetes, otherwise this will break the code in other environments
+    //The above is for Openshift or K8, the below is for local
+    //'mykubermongo' : 'localhost',
+	'mykubermongo' : 'mymongo',       //Comment out if not using Kubernetes, Reverting to mymongo
     'other' : ''
 }
