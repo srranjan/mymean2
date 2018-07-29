fdbd938bfba90f42f5faf8593260556f54c811fe
Rajiv Ranjan <rajiv.ranjan@ieee.org>
2018-07-29 16:52:49 -0400

    mongodb resotred instead of myomongo

--- routes/config.js
+++ routes/config.js
@@ -5,10 +5,10 @@
 
 module.exports = {
     'remoteUrl' : 'http://localhost:9091/clients/',
-      //Value below changed from mymong to mongdb-persisten for openshift template requirement
-	//'mykubermongo' : 'mongodb',       //Comment out if not using Kubernetes, otherwise this will break the code in other environments
+      //Value below changed from mymongo to mongdb-persistent val for openshift template requirement
+	'mykubermongo' : 'mongodb',       //Comment out if not using Kubernetes, otherwise this will break the code in other environments
     //The above is for Openshift or K8, the below is for local
     //'mykubermongo' : 'localhost',
-	'mykubermongo' : 'mymongo',       //Comment out if not using Kubernetes, Reverting to mymongo
+	//'mykubermongo' : 'mymongo',       //Comment out if not using Kubernetes, Reverting to mymongo
     'other' : ''
 }
